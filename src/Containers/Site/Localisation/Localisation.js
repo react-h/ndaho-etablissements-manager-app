eea72b1ce7e3c2c895231b0639fb810506a5d13b
ndayiho <ndayiho5@gmail.com>
2021-05-27 13:03:26 +0200

    à ameliorer plus tard

--- src/Containers/Site/Localisation/Localisation.js
+++ src/Containers/Site/Localisation/Localisation.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import useFetchData from '../../../hooks/useFetchData';
 
-const Localisation = (props) => (
-    <>
-        Localisation
-    </>
-);
+const Localisation = (props) => {
+const dataInfos=useFetchData("https://etablissements-publics.api.gouv.fr/v3/departements/35/mairie")
+console.log(dataInfos)
+    return (
+        <>
+            Localisation ---# deja fait sur l'ancien version
+        </>
+    )
+}
+
 
 export default Localisation;
